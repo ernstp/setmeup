67b57f846d71453867a6b7bd5e92e55e8fb82745
Ernst Sjöstrand <ernst.sjostrand@verisure.com>
2018-02-14 13:02:55 +0100

    Best gnome settings

---
+++ gnome.sh
@@ -0,0 +1,13 @@
+gsettings set org.gnome.shell disable-extension-version-validation true
+
+gsettings set org.gnome.desktop.wm.preferences button-layout "close,minimize,maximize:appmenu"
+gsettings set org.gnome.desktop.wm.preferences mouse-button-modifier "<Alt>"
+
+gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Super>x']"
+gsettings set org.gnome.desktop.wm.keybindings minimize "['<Super>z']"
+gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>Up']"
+
+gsettings set org.gnome.desktop.interface clock-show-date true
+gsettings set org.gnome.desktop.interface enable-animations false
+
+gsettings set org.gnome.evolution.calendar show-week-numbers true
